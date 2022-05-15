6ff00349b38ba3232cf6ab09d61518fa283b3a53
Jim Finnis <jim.finnis@gmail.com>
2022-05-15 11:03:29 +0100

    better params

--- src/CVFold.cpp
+++ src/CVFold.cpp
@@ -20,16 +20,16 @@ struct CVFold : Module {
     
     CVFold() {
         config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-        configParam(OCT_PARAM, 0.f, 1.f, 0.f, "");
-        configInput(IN_INPUT, "");
-        configOutput(OUT_OUTPUT, "");
+        configParam(OCT_PARAM, -3.0f, 3.0f, 0.0f, "Octave");
+        configInput(IN_INPUT, "1V/octave pitch input");
+        configOutput(OUT_OUTPUT, "1V/octave pitch output");
     }
     
     void process(const ProcessArgs& args) override {
         static int prevn=-1;
         float pitch = inputs[IN_INPUT].getVoltage();
         float octF = params[OCT_PARAM].getValue();
-        int oct = (int)(octF*5);
+        int oct = (int)(std::round(octF));
         // 1V/oct
         int note = (int)((pitch*12.0f)+0.001f);
         if(note!=prevn){
@@ -38,7 +38,7 @@ struct CVFold : Module {
         }
         
         note = note%12; // remove octave
-        note += (oct-3)*12;
+        note += oct*12;
         
         pitch = ((float)note) / 12.0f;
 
@@ -58,11 +58,16 @@ struct CVFoldWidget : ModuleWidget {
         addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
         addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
         
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(36.906, 62.063)), module, CVFold::OCT_PARAM));
+        RoundBlackKnob *octParam = createParamCentered<RoundBlackKnob>(mm2px(Vec(36.906, 62.063)), module, CVFold::OCT_PARAM);
+        octParam->getParamQuantity()->smoothEnabled=false;
+        octParam->getParamQuantity()->snapEnabled=true;
+        addParam(octParam);
         
         addInput(createInputCentered<PJ301MPort>(mm2px(Vec(37.375, 29.681)), module, CVFold::IN_INPUT));
         
         addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(38.226, 98.25)), module, CVFold::OUT_OUTPUT));
+        
+        
     }
 };
 
