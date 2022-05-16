01f998f0b5ca66f08419cd093bb12e8cd46b3f35
Jim Finnis <jim.finnis@gmail.com>
2022-05-16 16:22:38 +0100

    Modules use gencomponents to build component indices and coords into a separate .h file.

--- src/PolySelect.cpp
+++ src/PolySelect.cpp
@@ -1,26 +1,10 @@
 #include "plugin.hpp"
+#include "PolySelectComponents.h"
 
 struct PolySelect : Module {
-    enum ParamId {
-        MUL_PARAM,
-              PARAMS_LEN
-          };
-    enum InputId {
-        POLY_INPUT,
-              SELECT_INPUT,
-              INPUTS_LEN
-          };
-    enum OutputId {
-        CV_OUTPUT,
-              OUTPUTS_LEN
-          };
-    enum LightId {
-        LIGHTS_LEN
-          };
-    
     PolySelect() {
         config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-        configParam(MUL_PARAM, 0.0f, 20.0f, 1.0f, "Multiplier applied to select input");
+        configParam(MULT_PARAM, 0.0f, 20.0f, 1.0f, "Multiplier applied to select input");
         configInput(SELECT_INPUT, "Select integer input");
         configInput(POLY_INPUT, "Polyphonic CV input");
         configOutput(CV_OUTPUT, "CV output");
@@ -30,7 +14,7 @@ struct PolySelect : Module {
         if(outputs[CV_OUTPUT].isConnected()){
             if(inputs[POLY_INPUT].isConnected()){
                 int channels = std::max(inputs[POLY_INPUT].getChannels(),1);
-                float mul = params[MUL_PARAM].getValue();
+                float mul = params[MULT_PARAM].getValue();
                 float sel = inputs[SELECT_INPUT].getVoltage();
                 
                 int select = (int)std::round(sel*mul);
@@ -67,10 +51,10 @@ struct PolySelectWidget : ModuleWidget {
         addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
         addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
         
-        addParam(snap(module,createParamCentered<KNOBSTYLE>(mm2px(Vec(39.9, 73.1)), module, PolySelect::MUL_PARAM)));
-        addInput(createInputCentered<PJ301MPort>(mm2px(Vec(39.9, 22.7)), module, PolySelect::POLY_INPUT));
-        addInput(createInputCentered<PJ301MPort>(mm2px(Vec(39.9, 54.15)), module, PolySelect::SELECT_INPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(39.9, 113.0)), module, PolySelect::CV_OUTPUT));
+        addParam(snap(module,createParamCentered<KNOBSTYLE>(mm2px(MULT_PARAM_POS), module, MULT_PARAM)));
+        addInput(createInputCentered<PJ301MPort>(mm2px(POLY_INPUT_POS), module, POLY_INPUT));
+        addInput(createInputCentered<PJ301MPort>(mm2px(SELECT_INPUT_POS), module, SELECT_INPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(CV_OUTPUT_POS), module, CV_OUTPUT));
     }
 };
 
---
+++ src/PolySelectComponents.h
@@ -0,0 +1,28 @@
+static Vec MULT_PARAM_POS = Vec(39.864,73.135);
+
+static Vec POLY_INPUT_POS = Vec(39.864,22.695);
+static Vec SELECT_INPUT_POS = Vec(39.864,54.159);
+
+static Vec CV_OUTPUT_POS = Vec(39.864,113.066);
+
+
+enum ParamId {
+MULT_PARAM,
+PARAMS_LEN
+};
+
+enum InputId {
+POLY_INPUT,
+SELECT_INPUT,
+INPUTS_LEN
+};
+
+enum OutputId {
+CV_OUTPUT,
+OUTPUTS_LEN
+};
+
+enum LightId {
+LIGHTS_LEN
+};
+
