d8883f99f9a93b5f43b74a5a13e1c1a0e15a520a
Jim Finnis <jim.finnis@gmail.com>
2022-05-24 11:07:13 +0100

    working on Chord2 which does fadeup and down, but is more expensive in terms of poly channels.

---
+++ src/Chord2Components.h
@@ -0,0 +1,54 @@
+static Vec RATE_PARAM_POS = Vec(32.363,27.543);
+static Vec N12_PARAM_POS = Vec(14.397,28.536);
+static Vec N11_PARAM_POS = Vec(14.397,37.437);
+static Vec N10_PARAM_POS = Vec(23.867,41.159);
+static Vec N9_PARAM_POS = Vec(14.397,46.338);
+static Vec N8_PARAM_POS = Vec(23.867,50.06);
+static Vec N7_PARAM_POS = Vec(14.397,55.239);
+static Vec N6_PARAM_POS = Vec(23.867,58.961);
+static Vec N5_PARAM_POS = Vec(14.397,64.14);
+static Vec N4_PARAM_POS = Vec(14.397,73.041);
+static Vec N3_PARAM_POS = Vec(23.867,76.763);
+static Vec N2_PARAM_POS = Vec(14.397,81.941);
+static Vec N1_PARAM_POS = Vec(23.867,85.664);
+static Vec N0_PARAM_POS = Vec(14.397,90.842);
+
+static Vec ROOT_INPUT_POS = Vec(19.688,17.307);
+
+static Vec POLY_OUTPUT_POS = Vec(11.843,105.196);
+static Vec CVOUT_OUTPUT_POS = Vec(29.713,105.196);
+
+
+enum ParamId {
+N12_PARAM,
+N11_PARAM,
+N10_PARAM,
+N9_PARAM,
+N8_PARAM,
+N7_PARAM,
+N6_PARAM,
+N5_PARAM,
+N4_PARAM,
+N3_PARAM,
+N2_PARAM,
+N1_PARAM,
+N0_PARAM,
+RATE_PARAM,
+PARAMS_LEN
+};
+
+enum InputId {
+ROOT_INPUT,
+INPUTS_LEN
+};
+
+enum OutputId {
+POLY_OUTPUT,
+CVOUT_OUTPUT,
+OUTPUTS_LEN
+};
+
+enum LightId {
+LIGHTS_LEN
+};
+
